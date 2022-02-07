f93728ac09f7f403ab7dd14d0c36cae5791ffa1b
austinthao5 <59717878+austinthao5@users.noreply.github.com>
2022-02-07 14:25:45 -0800

    Update main.tf

--- terraform-samples-master/austin-test/main.tf
+++ terraform-samples-master/austin-test/main.tf
@@ -9,9 +9,23 @@ module "s3_bucket" {
   versioning = {
     enabled = false
   }
+  
+  server_side_encryption_configuration = {
+    rule  = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 
   tags = {
     env   = "dev"
     owner = "austin"
   }
+}
+
+data "aws_s3_bucket" "selected" {
+  depends_on = [ module.s3_bucket ]
+
+  bucket = "test-bucket-austin"
 }
